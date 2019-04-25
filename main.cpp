9b3d2aa4be8f864902e7cc18c6fe5468d02d029c
rokas070 <47443104+rokas070@users.noreply.github.com>
2019-04-25 21:08:06 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,21 +2,28 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::vector;
 using std::string;
+using std::setw;
+using std::setfill;
+using std::left;
+using std::right;
+using std::setprecision;
 
 struct irasas{
     string var, pav;
     int egz;
     vector <int> paz;
-    double vid, med, rez;
+    double vid, med, rez1, rez2;
     void Vid();
     void Med();
-    void Rez();
+    void Rez1();
+    void Rez2();
 };
 
 int main()
@@ -34,23 +41,23 @@ int main()
             else{cout<<"Netinkamas pazymys, bandykite is naujo: ";}
             cin>>temp_nd;
         }
-     temp.Vid(); temp.Med(); temp.Rez();
+     temp.Vid(); temp.Med(); temp.Rez1();temp.Rez2();
      duomenubaze.push_back(temp);
-     cout<<"Ar norite ivesti kita? t/kitas kl. ";
+     cout<<"Ar norite ivesti kita? (t/kitas kl.) ";
      cin>>pakl;
      }while (pakl == 't');
+     cout<<"Rezultatui skaiciuoti naudojama vidurkis ar mediana? (v/m) ";
+     char h;
+     cin>>h;
 
+     cout<<setfill(' ')<<setw(12)<<left<<"Vardas"<<setw(13)<<left<<"Pavarde"<<right<<"Galutinis (Vid.)/Galutinis (Med.)"<<endl;
+     cout<<setfill('-')<<setw(58)<<'-'<<endl;
      for (irasas a: duomenubaze)
      {
-         cout<<"Vardas: "<<a.var<<endl;
-         cout<<"Pavarde: "<<a.pav<<endl;
-         cout<<"Egzaminas: "<<a.egz<<endl;
-         for(int p: a.paz)
-            cout<<p<<" : ";
-         cout<<endl;
-         cout<<"Vid.: "<<a.vid<<endl;
-         cout<<"Med.: "<<a.med<<endl;
-         cout<<"Rez.: "<<a.rez<<endl;
+         if (h=='m')
+            cout<<setfill(' ')<<setw(12)<<left<<a.var<<setw(25)<<left<<a.pav<<right<<a.rez2<<setprecision(3)<<endl;
+         else
+         cout<<setfill(' ')<<setw(12)<<left<<a.var<<setw(25)<<left<<a.pav<<right<<a.rez1<<setprecision(3)<<endl;
      }
 
 
@@ -66,6 +73,9 @@ int main()
         sort(paz.begin(), paz.end());
        med=(paz.size()%2==0) ? (paz[paz.size()/2-1]+paz[paz.size()/2])/2 : paz[paz.size()/2];
     }
-    void irasas::Rez(){
-        rez=0.4*vid+0.6*egz;
+    void irasas::Rez1(){
+        rez1=0.4*vid+0.6*egz;
+    }
+    void irasas::Rez2(){
+        rez2=0.4*med+0.6*egz;
     }
